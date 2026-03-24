9220ad1529f393d4f65c8ec8d241a18d36e8d1e1
A-PachecoT <andre.pacheco.t@uni.pe>
2026-03-24 02:47:49 +0100

    fix: switch to subdomain config (giselle-pantoja.cofoundy.dev)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-giselle-pantoja",
+  site: "https://giselle-pantoja.cofoundy.dev",
   vite: {
     plugins: [tailwindcss()],
   },
